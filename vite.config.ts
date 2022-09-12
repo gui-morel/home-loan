759fd9bb167448955466414344c5d5d53a6a2f98
Guillaume MOREL <guillaume.morel.dev@gmail.com>
2022-09-12 16:07:46 +0200

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/home-loan/',
   plugins: [react()],
-  server: {
-    base: '/home-loan/'
-  }
 })
