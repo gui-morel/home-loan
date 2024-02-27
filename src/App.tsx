e1baef14ec7478882e9b4af672b7315aacdf140b
Guillaume Morel <guillaume.morel763@gmail.com>
2024-02-27 02:35:10 +0100

    Routing is hard with github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {HomeLoan} from './components/home-loan/HomeLoan';
-import {Investment} from './components/financial-investment/Investment';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { HomeLoan } from './components/home-loan/HomeLoan';
+import { Investment } from './components/financial-investment/Investment';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
-import {Layout} from "./components/Layout";
+import { Layout } from "./components/Layout";
 
 const router = createBrowserRouter([
     {
@@ -12,16 +12,16 @@ const router = createBrowserRouter([
         element: <Layout/>,
         children: [
             {
-                path: "home-loan",
-                element: <HomeLoan></HomeLoan>
+                index: true,
+                element: <HomeLoan></HomeLoan>,
             },
             {
-                path: "investment",
+                path: "/investment",
                 element: <Investment></Investment>
             }
         ],
     },
-], {basename: "/home-loan"});
+]);
 
 function App() {
     return <RouterProvider router={router}/>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,29 +1,33 @@
 import React from "react";
-import {Link, Outlet} from "react-router-dom";
-import {Container, Nav, Navbar} from "react-bootstrap";
-import {AiOutlineStock} from "react-icons/ai";
-import {FaHouseCircleCheck} from "react-icons/fa6";
-import {ThemeToggle} from "./ThemeToggle";
+import { Link, Outlet } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { AiOutlineStock } from "react-icons/ai";
+import { FaHouseCircleCheck } from "react-icons/fa6";
+import { ThemeToggle } from "./ThemeToggle";
+
+const LayoutNav = () => {
+    return <Navbar expand="lg" className="bg-body-tertiary mb-3">
+        <Container fluid>
+            <Navbar.Brand>gui-morel</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll"/>
+            <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{maxHeight: '100px'}}
+                    navbarScroll
+                >
+                    <Nav.Link as={Link} to="/"><FaHouseCircleCheck/> Home Loan</Nav.Link>
+                    <Nav.Link as={Link} to="/investment"><AiOutlineStock/> Investment</Nav.Link>
+                </Nav>
+                <ThemeToggle/>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>;
+}
 
 export const Layout = () => {
     return <>
-        <Navbar expand="lg" className="bg-body-tertiary mb-3">
-            <Container fluid>
-                <Navbar.Brand>gui-morel</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll"/>
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{maxHeight: '100px'}}
-                        navbarScroll
-                    >
-                        <Nav.Link as={Link} to="/home-loan"><FaHouseCircleCheck/> Home Loan</Nav.Link>
-                        <Nav.Link as={Link} to="/investment"><AiOutlineStock/> Investment</Nav.Link>
-                    </Nav>
-                    <ThemeToggle/>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <LayoutNav></LayoutNav>
         <Container>
             <Outlet/>
         </Container>
